2f498d83103269441a549254af3028aef704a3ca
raffeyC <raffey2001@gmail.com>
2024-12-17 13:22:04 -0800

    Add code to detect device if device is None

--- torch_mesmer/mesmer.py
+++ torch_mesmer/mesmer.py
@@ -370,7 +370,38 @@ class Mesmer():
         'validation_steps_per_epoch': 193 // 1
     }
 
-    def __init__(self, model=None, device=torch.device("cpu")):
+    def __init__(self, model=None, device=None):
+
+        if device is None:
+            # select the device for computation
+            if torch.cuda.is_available():
+                device = torch.device("cuda")
+            elif torch.backends.mps.is_available():
+                import os
+                # if using Apple MPS, fall back to CPU for unsupported ops
+                os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
+                device = torch.device("mps")
+            else:
+                device = torch.device("cpu")
+            
+
+            # # This fails as bfloat16 cannot be converted to numpy
+            # # Is also slower during inference
+            # if device.type == "cuda":
+            #     # use bfloat16 for the entire notebook
+            #     torch.autocast("cuda", dtype=torch.bfloat16).__enter__()
+            #     # turn on tfloat32 for Ampere GPUs (https://pytorch.org/docs/stable/notes/cuda.html#tensorfloat-32-tf32-on-ampere-devices)
+            #     if torch.cuda.get_device_properties(0).major >= 8:
+            #         torch.backends.cuda.matmul.allow_tf32 = True
+            #         torch.backends.cudnn.allow_tf32 = True
+            # elif device.type == "mps":
+            #     print(
+            #         "\nSupport for MPS devices is preliminary. SAM 2 is trained with CUDA and might "
+            #         "give numerically different outputs and sometimes degraded performance on MPS. "
+            #         "See e.g. https://github.com/pytorch/pytorch/issues/84936 for a discussion."
+            #     )
+
+        print(f"using device: {device}")
 
         if model is None:
             raise Exception("Need to provide a model")
@@ -389,7 +420,8 @@ class Mesmer():
             # model = tf.keras.models.load_model(model_path)
 
         self.device = device
-        self.model = model
+        self.model = model.to(self.device)
+
 
         self.model_image_shape = model.input_shape[1:]
         # Require dimension 1 larger than model_input_shape due to addition of batch dimension
